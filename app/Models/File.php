c620a25ef0cee5f4d212a37c8fdb5fe5789478e3
kimchanhyung98 <kimchanhyung98@gmail.com>
2020-05-22 01:26:40 +0900

    Create models

---
+++ app/Models/File.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class File extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'download' => 0,
+    ];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * Defining The Inverse Of The Relationship
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'views' => 0,
+        'is_notice' => 0,
+    ];
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * Defining The Inverse Of The Relationship
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function file()
+    {
+        return $this->belongsTo(File::class);
+    }
+
+    /**
+     * Defining the scope to a given Eloquent query builder.
+     */
+
+    /**
+     * 전체 게시글, 조회수 순
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopePopular($query)
+    {
+        return $query->orderbyDesc('views');
+    }
+}
