88041bdd7970ff2d68a143103f34134d8970b539
kimchanhyung98 <kimchanhyung98@gmail.com>
2020-05-22 01:13:38 +0900

    Create migrations

--- database/migrations/2020_05_22_000000_create_files_table.php
+++ database/migrations/2020_05_22_000000_create_files_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateFilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('files', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name')->comment('원본 파일명');
+            $table->string('path')->comment('파일 경로');
+            $table->unsignedInteger('download')->default(0)->comment('다운로드 횟수');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('files');
     }
 }
