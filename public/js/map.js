2de7aa9d840eb55732040391107d7120bab98906
kimchanhyung98 <kimchanhyung98@gmail.com>
2020-05-22 01:40:39 +0900

    update map

--- public/js/map.js
+++ public/js/map.js
@@ -1,12 +1,16 @@
 $(function () {
     /* 네이버 지도 옵션 */
+    var center_lat = $('#center_lat').val();
+    var center_lng = $('#center_lng').val();
+
     var map = new naver.maps.Map('map', {
-        center: new naver.maps.LatLng(37.483770, 126.926146),
+        // center: new naver.maps.LatLng(center_lat, center_lng),
 
         scaleControl: false,
         logoControl: false,
         mapDataControl: false,
         zoomControl: true,
+        zoom: 13, //지도의 초기 줌 레벨
         minZoom: 6,
 
         mapTypeControl: true,
@@ -16,17 +20,18 @@ $(function () {
     });
 
     /* 마커 */
+    /*
     var marker = new naver.maps.Marker({
         position: new naver.maps.LatLng(37.5150345, 126.8841088),
         map: map
     });
+    */
 
     /* PATH 설정 */
     // var HOME_PATH = window.HOME_PATH || '.';
 
     /* 자전거 레이어 표시 */
     var bicycleLayer = new naver.maps.BicycleLayer();
-
     var btn = $('#bicycle');
 
     naver.maps.Event.addListener(map, 'bicycleLayer_changed', function (bicycleLayer) {
@@ -47,10 +52,12 @@ $(function () {
         }
     });
 
+    var gpx_url = $('#gpx_url').val();
+
     naver.maps.Event.once(map, 'init_stylemap', function () {
         /* GPX 파일 설정 */
         $.ajax({
-            url: '/map.gpx',
+            url: gpx_url,
             dataType: 'xml',
             success: startDataLayer
         });
