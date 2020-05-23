25d5e1ace28fd3ac4276469fe8c57d909b082830
kimchanhyung98 <kimchanhyung98@gmail.com>
2020-05-24 02:39:25 +0900

    Update gtag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,16 @@
 
     <title>{{ config('app.name', 'Bicycle Map') }}</title>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163179069-4"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-163179069-4');
+    </script>
+
     <!-- Scripts -->
     @yield('script', '')
 
