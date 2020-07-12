13ed36db5943114eb4cb9df94c889e45de4c361f
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-07-12 17:03:49 +0900

    header 디자인 작업

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -4,7 +4,19 @@ class Header extends Component {
     render() {
         return (
             <header className="header">
-                 Header
+                 <div className="header-wrap">
+                    <div className="gnb-area">
+                        <a href="#"></a>
+                    </div>
+
+                    <h1 className="header-logo">
+                        <a href="/">Ride</a>
+                    </h1>
+
+                    <div className="header-btn-area">
+                        <a href="#"></a>
+                    </div>
+                 </div>
             </header>
         );
     }
