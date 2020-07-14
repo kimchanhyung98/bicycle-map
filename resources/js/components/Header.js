00cfbf8bdfbaa483db5f8d673dc15ebbbbc28851
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-07-15 00:07:24 +0900

    Header 메뉴 작업

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -1,21 +1,74 @@
 import React, {Component} from 'react';
+import {Link, withRouter} from 'react-router-dom';
+
+const AuthMenuList = (props) => {
+    return [
+        <li><Link to="/">메인</Link></li>,
+        <li><Link to="/logout">로그아웃</Link></li>
+    ]
+};
+
+const MenuList = (props) => {
+    return [
+        <li><Link to="/">메인</Link></li>,
+        <li><Link to="/login">로그인</Link></li>,
+        <li><Link to="/register">회원가입</Link></li>
+    ]
+};
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isMenu: false,
+            user: {},
+            isLogin: false,
+        };
+
+        this.logout = this.logout.bind(this);
+        this.menuClick = this.menuClick.bind(this);
+    }
+
+    logout() {
+        let state = {
+            user: {},
+            isLogin: false
+        };
+        localStorage['appState'] = JSON.stringify(state);
+        this.setState(state);
+        this.props.history.push('/login');
+    }
+
+    menuClick() {
+        this.setState({
+            isMenu: !this.state.isMenu
+        });
+    }
+
     render() {
+        const isLogin = this.state.isLogin;
+        let Menu;
+
+        if (isLogin) {
+            Menu = <AuthMenuList />;
+        } else {
+            Menu = <MenuList />;
+        }
+
         return (
             <header className="header">
                 <div className="header-wrap">
                     <div className="gnb-area">
-                        <button type="button" className="btn-gnb"></button>
+                        <button type="button" className="btn-gnb" onClick={ this.menuClick }></button>
 
-                        <ul className="gnb-menu-list">
-                            <li><Link to="/login">로그인</Link></li>
-                            <li><Link to="/register">회원가입</Link></li>
-                        </ul>
+                        { this.state.isMenu ?
+                            <ul className="gnb-menu-list">{ Menu }</ul> : ''
+                        }
                     </div>
 
                     <h1 className="header-logo">
-                        <a href="/">Ride</a>
+                        <Link to="/">Ride</Link>
                     </h1>
 
                     <div className="header-btn-area">
