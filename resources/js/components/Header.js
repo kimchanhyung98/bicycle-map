9b416f3c8df8a5d2de84aaf194163b8a37920752
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-07-14 23:02:25 +0900

    헤더 메뉴 디자인 작업

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -4,9 +4,14 @@ class Header extends Component {
     render() {
         return (
             <header className="header">
-                 <div className="header-wrap">
+                <div className="header-wrap">
                     <div className="gnb-area">
-                        <a href="#"></a>
+                        <button type="button" className="btn-gnb"></button>
+
+                        <ul className="gnb-menu-list">
+                            <li><Link to="/login">로그인</Link></li>
+                            <li><Link to="/register">회원가입</Link></li>
+                        </ul>
                     </div>
 
                     <h1 className="header-logo">
@@ -16,7 +21,7 @@ class Header extends Component {
                     <div className="header-btn-area">
                         <a href="#"></a>
                     </div>
-                 </div>
+                </div>
             </header>
         );
     }
