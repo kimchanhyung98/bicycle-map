2a5413a5cc965cf5fc2b616eba6955ced7274395
kimchanhyung98 <kimchanhyung98@gmail.com>
2020-07-15 23:38:57 +0900

    Delete passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable, SoftDeletes;
+    use Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
