5638003300bc2777c69f54fe0c804f5f615fae4f
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-07-21 00:25:12 +0900

    코드 개선

--- resources/js/reducers/user.js
+++ resources/js/reducers/user.js
@@ -31,7 +31,7 @@ const userReducer = (state = {}, action) => {
             };
         default:
             return {
-                status: 'asdasd'
+                status: 'default'
             };
     }
 };
