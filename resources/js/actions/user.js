f5c6bcb6fbcac4970cfc1a49e5aaafac1fdf4cd0
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-07-23 23:51:04 +0900

    update actions

---
+++ resources/js/actions/user.js
@@ -0,0 +1,11 @@
+export const LOGIN = 'LOGIN';
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+export const login = (email, password) => {
+    return {
+        type: LOGIN,
+        promise: { method: 'post', url: '/api/login', data: { email, password } }
+    };
+};
