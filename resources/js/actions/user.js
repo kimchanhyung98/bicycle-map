58ef2a1644bdc42d8794c7e6b38767edf330f091
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-07-24 23:47:51 +0900

    Update user.js

--- resources/js/actions/user.js
+++ resources/js/actions/user.js
@@ -4,8 +4,35 @@ export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
 export const login = (email, password) => {
-    return {
-        type: LOGIN,
-        promise: { method: 'post', url: '/api/login', data: { email, password } }
-    };
+    return (dispatch) => {
+        dispatch(loginRequest());
+
+        return axios.post('/api/login', {
+            email,
+            password
+        }).then(res => {
+            dispatch(loginRequest(res));
+        }).catch(err => {
+            dispatch(loginFailure());
+        });
+    }
 };
+
+export function loginRequest() {
+    return {
+        type: LOGIN_REQUEST
+    };
+}
+
+export function loginSuccess(user) {
+    return {
+        type: LOGIN_SUCCESS,
+        user
+    };
+}
+
+export function loginFailure() {
+    return {
+        type: LOGIN_FAILURE
+    };
+}
