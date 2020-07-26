a60b9e7a5fbe81b57642b5fef73cccf236a156bb
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-07-26 20:26:15 +0900

    layout 코드 개선

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,13 @@
-require('@/bootstrap');
-require('@/Index');
+import '@/bootstrap';
+
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from '@/reducers/user.js';
+import thunk from 'redux-thunk';
+
+import Index from '@/Index';
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(<Provider store = {store}><Index /></Provider>, document.getElementById('app'));
