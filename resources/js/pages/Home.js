03f07fb68cb63f97c66cfc6d179c030e1b143431
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-08-06 23:43:13 +0900

    메인 페이지 디자인 작업

--- resources/js/pages/Home.js
+++ resources/js/pages/Home.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
+import '@sass/pages/index.scss';
+
 const mapStateToProps = (state) => ({
     state
 });
@@ -14,11 +16,52 @@ class Home extends Component {
         let data = this.props.state;
 
         return (
-            <div>
-                <h2>
-                    홈
-                </h2>
-            </div>
+            <article className="main-container">
+                <ul className="ride-list">
+                    <li>
+                        <a href="#">
+                            <div className="left-area">
+                                <h2 className="ride-title">제목 ㅁㄴㅇㅁㄴㅇ</h2>
+                                <div className="ride-detail">
+                                    <span>초급</span>
+                                    <span>거리 100km</span>
+                                    <span>고도 1000m</span>
+                                </div>
+                                <div className="ride-detail">
+                                    <span>봉천로 123-4</span>
+                                    <span>출발시간</span>
+                                </div>
+                            </div>
+
+                            <div className="right-area">
+                                <time className="ride-date">2020-07-01 10:10</time>
+                                <span className="ride-attend">1명 참석</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div className="left-area">
+                                <h2 className="ride-title">제목 ㅁㄴㅇㅁㄴㅇ</h2>
+                                <div className="ride-detail">
+                                    <span>초급</span>
+                                    <span>거리 100km</span>
+                                    <span>고도 1000m</span>
+                                </div>
+                                <div className="ride-detail">
+                                    <span>봉천로 123-4</span>
+                                    <span>출발시간</span>
+                                </div>
+                            </div>
+
+                            <div className="right-area">
+                                <time className="ride-date">2020-07-01 10:10</time>
+                                <span className="ride-attend">1명 참석</span>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </article>
         );
     }
 };
