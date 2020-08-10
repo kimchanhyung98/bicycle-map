c1d43120dfa505376468c012d4473ae1b6ab9746
kimchanhyung98 <kimchanhyung98@gmail.com>
2020-08-10 20:52:22 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.app');
-});
+Auth::routes([
+    'verify' => true,
+    'reset' => false
+]);
 
-Auth::routes();
+Route::view('{path?}', 'layouts.app');
