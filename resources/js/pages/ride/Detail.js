bf7a338158c4bb8c6fa41b6af33f23325754f781
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-08-10 23:46:50 +0900

    상세 페이지 마크업 작업

--- resources/js/pages/ride/Detail.js
+++ resources/js/pages/ride/Detail.js
@@ -13,27 +13,74 @@ class Home extends Component {
         super(props);
     }
 
-    getData() {
-        axios.get('/api/rides').then(res => {
-            this.setState({
-                rides: res.data.rides.data
-            });
-            console.log(res);
-        }).catch(err => {
-            console.log(err);
-        })
-    }
-
     componentDidMount() {
-        this.getData();
     }
 
     render() {
-        let rides = Array.from(this.state.rides);
-
         return (
-            <article className="main-container">
-            </article>
+            <div>
+                <section className="map-container"></section>
+                <section className="main-container">
+                    <div className="ride-header">
+                        <div className="ride-attend">EAsY</div>
+                        <a href="#" className="btn-share">공유</a>
+                    </div>
+
+                    <div className="ride-content">
+                        <div>
+                            <h2 className="ride-title">제목 ㅁㄴㅇㅁㄴㅇㅁ</h2>
+                        </div>
+
+                        <div>
+                            <div className="img-area">
+                                <img src="" alt="" />
+                            </div>
+
+                            <div className="user-info-area">
+                                <span className="user-name">이름</span>
+                                <p className="user-summary">
+                                    소개글
+                                </p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="ride-detail">
+                                <span>거리 100km</span>
+                                <span>획득고도 1000m</span>
+                            </div>
+
+                            <div className="ride-date">
+                                시간 2020.7.1 (수) 09:00 (3시간)
+                            </div>
+
+                            <div className="ride-address">
+                                <span>
+                                    장소 봉청로 1234-1
+                                </span>
+                                <span>
+                                    상세 (옵션) asdasd
+                                </span>
+                            </div>
+                        </div>
+
+                        <div>
+                            <p>
+                                소개긂ㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ
+                            </p>
+                        </div>
+
+                        <div>
+                            <span>정원 10명</span>
+                            <span>현재 5명 참석</span>
+                        </div>
+                    </div>
+
+                    <div className="btn-area">
+                        <button type="button">참가 하기</button>
+                    </div>
+                </section>
+            </div>
         );
     }
 };
