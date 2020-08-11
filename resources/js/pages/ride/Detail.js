c45a6e2a135e25632ff6b7ea628949af3f51252b
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-08-11 22:55:55 +0900

    Update Detail.js

--- resources/js/pages/ride/Detail.js
+++ resources/js/pages/ride/Detail.js
@@ -11,24 +11,46 @@ const mapStateToProps = (state) => ({
 class Home extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            id: this.props.match.params.id,
+            ride: {}
+        }
+    }
+
+    getData() {
+        axios.get(`/api/ride/${this.state.id}`).then(res => {
+            console.log(res);
+            this.setState({
+                ride: res.data.ride
+            });
+
+            console.log(this.state);
+        }).catch(err => {
+            console.log(err);
+        })
     }
 
     componentDidMount() {
+        this.getData();
     }
 
     render() {
+        let ride = JSON.stringify(this.state.ride);
+
         return (
             <div>
+                {ride}
                 <section className="map-container"></section>
                 <section className="main-container">
                     <div className="ride-header">
-                        <div className="ride-attend">EAsY</div>
+                        <div className="ride-difficulty">{ ride.difficulty }</div>
                         <a href="#" className="btn-share">공유</a>
                     </div>
 
                     <div className="ride-content">
                         <div>
-                            <h2 className="ride-title">제목 ㅁㄴㅇㅁㄴㅇㅁ</h2>
+                            <h2 className="ride-title">{ ride.name }</h2>
                         </div>
 
                         <div>
@@ -37,41 +59,42 @@ class Home extends Component {
                             </div>
 
                             <div className="user-info-area">
-                                <span className="user-name">이름</span>
+                                <span className="user-name">{ ride.user }</span>
                                 <p className="user-summary">
-                                    소개글
+                                    { ride.user }
                                 </p>
                             </div>
                         </div>
 
                         <div>
                             <div className="ride-detail">
-                                <span>거리 100km</span>
-                                <span>획득고도 1000m</span>
+                                <span>거리 { ride.distance }km</span>
+                                <span>획득고도 { ride.altitude }km</span>
                             </div>
 
                             <div className="ride-date">
-                                시간 2020.7.1 (수) 09:00 (3시간)
+                                { ride.started_at } ~ { ride.ended_at }
+                                /*시간 2020.7.1 (수) 09:00 (3시간) */
                             </div>
 
                             <div className="ride-address">
                                 <span>
-                                    장소 봉청로 1234-1
+                                    장소 { ride.address }
                                 </span>
                                 <span>
-                                    상세 (옵션) asdasd
+                                    상세 { ride.address_detail }
                                 </span>
                             </div>
                         </div>
 
                         <div>
                             <p>
-                                소개긂ㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ
+                                { ride.description }
                             </p>
                         </div>
 
                         <div>
-                            <span>정원 10명</span>
+                            <span>정원 { ride.capacity }명</span>
                             <span>현재 5명 참석</span>
                         </div>
                     </div>
