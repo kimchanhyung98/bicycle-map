8f06e14325b2e8a9924ba705387c6faa937beca9
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-08-12 23:01:08 +0900

    scss 파일명 수정

--- resources/js/pages/ride/Detail.js
+++ resources/js/pages/ride/Detail.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
 
-import '@sass/pages/ride.scss';
+import '@sass/pages/ride/ride-detail.scss';
 
 const mapStateToProps = (state) => ({
     state
