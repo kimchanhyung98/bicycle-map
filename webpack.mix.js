c27acc43bd1590f407ac9ba025c8d54ceb18b469
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-08-15 23:50:58 +0900

    scss scoped 추가

--- webpack.mix.js
+++ webpack.mix.js
@@ -23,25 +23,11 @@ mix.webpackConfig({
     module: {
         rules: [
             {
-              test: /\.(sc|c|sa)ss$/,
-              use: [
-                {
-                  loader: 'style-loader',
-                },
-                {
-                  loader: 'css-loader',
-                  options: {
-                    sourceMap: true,
-                    importLoaders: 2,
-                  },
-                },
-                // You have to put in after `css-loader` and before any `pre-precessing loader`
-                { loader: 'scoped-css-loader' },
-                {
-                  loader: 'sass-loader',
-                },
-              ],
+                test: /\.(sc|c|sa)ss$/,
+                use: [
+                    { loader: 'scoped-css-loader' },
+                ],
             },
         ]
     }
-}).sourceMaps();
+});
