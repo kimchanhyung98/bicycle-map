e757a0c1712bcbfd7dabb517cfda00203fa5f227
kimchanhyung98 <kimchanhyung98@gmail.com>
2020-08-16 23:08:06 +0900

    Create ParticipantController.php

---
+++ app/Http/Controllers/ParticipantController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Participant;
+use App\Models\Ride;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ParticipantController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request)
+    {
+        Participant::where([
+            'user_id' => $request->user()->id,
+            'ride_id' => $request->ride_id,
+        ])->firstOrCreate();
+
+        return response()->json([
+            'message' => '참가 신청되었습니다.',
+        ]);
+    }
+}
