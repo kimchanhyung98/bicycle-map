934de6ff9f1a7c7a075b833dd90f8cb52795c2db
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-08-18 23:24:42 +0900

    수정 페이지 추가

--- resources/js/Router.js
+++ resources/js/Router.js
@@ -4,6 +4,7 @@ import Home from '@/pages/Home';
 import Login from '@/pages/auth/Login';
 import Register from '@/pages/auth/Register';
 import RideCreate from '@/pages/ride/Create';
+import RideEdit from '@/pages/ride/Edit';
 import RideDetail from '@/pages/ride/Detail';
 
 const Main = props => (
@@ -12,6 +13,7 @@ const Main = props => (
         <Route path='/login' component={Login}/>
         <Route path='/register' component={Register}/>
         <Route path='/ride/create' component={RideCreate}/>
+        <Route path='/ride/edit' component={RideEdit}/>
         <Route path='/ride/:id' component={RideDetail}/>
     </Switch>
 );
