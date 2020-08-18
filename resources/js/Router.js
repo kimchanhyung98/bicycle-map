e8dfb14f53fcfb07887ed212341f81cd6ab88f0f
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-08-18 23:21:25 +0900

    개설 페이지 추가

--- resources/js/Router.js
+++ resources/js/Router.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
 import Home from '@/pages/Home';
 import Login from '@/pages/auth/Login';
 import Register from '@/pages/auth/Register';
+import RideCreate from '@/pages/ride/Create';
 import RideDetail from '@/pages/ride/Detail';
 
 const Main = props => (
@@ -10,6 +11,7 @@ const Main = props => (
         <Route exact path='/' component={Home}/>
         <Route path='/login' component={Login}/>
         <Route path='/register' component={Register}/>
+        <Route path='/ride/create' component={RideCreate}/>
         <Route path='/ride/:id' component={RideDetail}/>
     </Switch>
 );
