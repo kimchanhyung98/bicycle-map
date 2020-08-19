7e83c69d79407a0eb05a74ad44afe3d826f3b127
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-08-19 23:52:57 +0900

    개설 기능 작업

--- resources/js/pages/ride/Create.js
+++ resources/js/pages/ride/Create.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import {Link} from 'react-router-dom';
-
 
 const mapStateToProps = (state) => ({
     state
@@ -10,55 +8,111 @@ const mapStateToProps = (state) => ({
 class Create extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            name: 'asdasd',
+            description: 'asdasd',
+            started_at: '2020-06-25 20:17:25',
+            ended_at: '',
+
+            address: 'asdasd',
+            address_detail: '',
+            locality: '',
+            sublocality1: '',
+            sublocality2: '',
+            latitude: '',
+            longitude: '',
+
+            course: '',
+            difficulty: 'advanced',
+            capacity: '123123',
+            distance: '',
+            altitude: '123123',
+            altitude_detail: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        let nextState = {};
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState);
     }
 
     handleSubmit(e) {
         e.preventDefault();
-    }
 
-    componentDidMount() {
+        axios.post('/api/ride/store', this.state).then(res => {
+            alert(res.data.message);
+            this.props.history.push(`/ride/${res.data.ride_id}`);
+        }).catch(err => {
+            if (err.response.status == 422) {
+                const messages = err.response.data.errors;
+                alert(messages[Object.keys(messages)[0]]);
+            } else {
+                alert('오류');
+            }
+        })
+        alert('asdasd');
+
+        console.log(this.state);
     }
 
     render() {
         return (
             <main className="main">
                 <section className="create-container">
-                    <form>
+                    <form onSubmit={ this.handleSubmit}>
                         <div className="ride-name">
                             <label className="form-label">제목</label>
 
-                            <input type="text" name="name" placeholder="내용을 입력해주세요" />
+                            <input type="text" name="name" placeholder="내용을 입력해주세요"
+                                   onChange={this.handleChange} />
                         </div>
 
                         <div className="ride-description">
                             <label className="form-label">설명</label>
 
-                            <textarea name="description" placeholder="내용을 입력해주세요"></textarea>
+                            <textarea name="description" placeholder="내용을 입력해주세요"
+                                      onChange={this.handleChange}></textarea>
                         </div>
 
                         <div className="ride-date">
                             <label className="form-label">시간</label>
 
-                            <select name="started_at">
+                            <select name="started_at"
+                                    onChange={this.handleChange}>
                                 <option>11</option>
                                 <option>22</option>
                             </select>
-                            <select name="ended_at">
+                            <select name="ended_at"
+                                    onChange={this.handleChange}>
                                 <option>11</option>
                                 <option>22</option>
                             </select>
+                        </div>
+
+                        <div className="ride-address">
+                            <label className="form-label">장소</label>
+
+                            <input type="text" name="address" placeholder="장소를 입력해주세요"
+                                   onChange={this.handleChange} />
                         </div>
 
                         <div className="ride-course">
                             <label className="form-label">코스</label>
 
-                            <input type="file" name="course" placeholder="GPX 파일을 업로드해주세요" />
+                            <input type="file" name="course" placeholder="GPX 파일을 업로드해주세요"
+                                   onChange={this.handleChange} />
                         </div>
 
                         <div className="ride-difficulty">
                             <label className="form-label">난이도</label>
 
-                            <select name="difficulty">
+                            <select name="difficulty"
+                                    onChange={this.handleChange}>
                                 <option>11</option>
                                 <option>22</option>
                             </select>
@@ -67,7 +121,8 @@ class Create extends Component {
                         <div className="ride-capacity">
                             <label className="form-label">정원</label>
 
-                            <select name="capacity">
+                            <select name="capacity"
+                                    onChange={this.handleChange}>
                                 <option>11</option>
                                 <option>22</option>
                             </select>
@@ -76,7 +131,8 @@ class Create extends Component {
                         <div className="ride-distance">
                             <label className="form-label">거리</label>
 
-                            <select name="distance">
+                            <select name="distance"
+                                    onChange={this.handleChange}>
                                 <option>11</option>
                                 <option>22</option>
                             </select>
@@ -85,7 +141,8 @@ class Create extends Component {
                         <div className="ride-altitude">
                             <label className="form-label">고도</label>
 
-                            <select name="altitude">
+                            <select name="altitude"
+                                    onChange={this.handleChange}>
                                 <option>11</option>
                                 <option>22</option>
                             </select>
