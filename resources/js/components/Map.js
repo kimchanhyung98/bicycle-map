ef6b43c30c71afe533cc654753549af07b04e501
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-08-22 22:55:38 +0900

    지도 마커 작업
    - 클릭시 마커 수정 및 센터 이동
    - 코드 개선

--- resources/js/components/Map.js
+++ resources/js/components/Map.js
@@ -1,25 +1,49 @@
-import React from "react";
+import React, {Component} from 'react';
 import { RenderAfterNavermapsLoaded, NaverMap, Marker } from "react-naver-maps";
 
-export const Map = (props) => {
-    const NAVER_API_KEY = 'pcpokmfv1u';
+class Map extends Component {
+    constructor(props) {
+        super(props);
+    }
 
-    return (
-        <RenderAfterNavermapsLoaded
-            ncpClientId={NAVER_API_KEY}
-            error={<p>Maps Load Error</p>}
-            loading={<p>Maps Loading...</p>}>
-            <NaverMap
-                mapDivId={"map"} // default: react-naver-map
-                style={{
-                  width: props.width,
-                  height: props.height
-                }}
-                defaultCenter={{ lat: props.lat, lng: props.lng }} // 지도 초기 위치
-                zoom={10}>
-            </NaverMap>
-        </RenderAfterNavermapsLoaded>
-    );
+    render() {
+        const NAVER_API_KEY = 'pcpokmfv1u';
+
+        return (
+            <RenderAfterNavermapsLoaded
+                ncpClientId={NAVER_API_KEY}
+                error={<p>Maps Load Error</p>}
+                loading={<p>Maps Loading...</p>}>
+                <NaverMap
+                    mapDivId={'map'} // default: react-naver-map
+                    style={{
+                      width: this.props.width,
+                      height: this.props.height
+                    }}
+                    defaultCenter={{
+                        lat: this.props.lat,
+                        lng: this.props.lng
+                    }}
+                    center={{
+                        lat: this.props.lat,
+                        lng: this.props.lng
+                    }}
+                    zoom={ this.props.zoom }
+                    onClick={(e) => {
+                        this.props.handleChangeMarker(e.coord.y, e.coord.x);
+                    }}>
+
+                    <Marker
+                        position={{ lat: this.props.lat, lng: this.props.lng }}
+                        onClick={(e) => {
+                            this.se
+                            console.log(e);
+                            alert('여기는 네이버 입니다.')
+                        }} />
+                </NaverMap>
+            </RenderAfterNavermapsLoaded>
+        );
+    }
 };
 
 export default Map;
