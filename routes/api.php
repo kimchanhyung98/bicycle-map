0d1ac48445d6b8ef34137d62b1e87c2d4cd7a8ad
kimchanhyung98 <kimchanhyung98@gmail.com>
2020-08-30 15:01:50 +0900

    Create statuscontroller

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', 'Auth\AuthController@login');
 
 Route::middleware('auth:sanctum')->group(function () {
-    // 유저
-    Route::get('user', 'Auth\AuthController@user')->name('user');
+    Route::group(['prefix' => 'status', 'as' => 'status.'], function () {
+        // 유저 상태
+        Route::get('user', 'StatusController@user')->name('user');
+
+        // 라이드 참가 상태
+        Route::get('participation', 'StatusController@participation')->name('participation');
+    });
 
     // 파일 업로드
     Route::post('upload', 'FileController@store')->name('upload');
