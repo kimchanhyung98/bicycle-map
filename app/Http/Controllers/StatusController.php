1bbf72fd0a0bdf2d14210e43fb11ac7ca2f1b5a4
kimchanhyung98 <kimchanhyung98@gmail.com>
2020-08-30 15:30:46 +0900

    Update StatusController.php

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Models\Participant;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
