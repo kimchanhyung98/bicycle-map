334dc967ee544fb6200d65d76dc44362cbc2fc24
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-08-31 23:18:01 +0900

    상세 페이지 지도 작업
    - gpx 파일 출력 작업
    - 코드 개선

--- resources/js/pages/ride/Detail.js
+++ resources/js/pages/ride/Detail.js
@@ -81,107 +81,118 @@ class Detail extends Component {
 
         return (
             <main className="main">
-                <section className="map-container">
-                    <RenderAfterNavermapsLoaded
-                        ncpClientId={NAVER_API_KEY}
-                        error={<p>Maps Load Error</p>}
-                        loading={<p>Maps Loading...</p>}>
-                        <Map width={'100%'}
-                           height={'360px'}
-                           disabled={true}
-                           zoom={14}
-                           center={{
-                               lat: ride.latitude,
-                               lng: ride.longitude
+                <RenderAfterNavermapsLoaded
+                    ncpClientId={NAVER_API_KEY}
+                    error={<p>Maps Load Error</p>}
+                    loading={<p>Maps Loading...</p>}>
+                    <section className="map-container">
+                        <Map id={'map'}
+                            width={'100%'}
+                            height={'360px'}
+                            disabled={true}
+                            zoom={14}
+                            center={{
+                                lat: ride.latitude,
+                                lng: ride.longitude
+                            }}
+                            markers={[
+                                {
+                                    lat: ride.latitude,
+                                    lng: ride.longitude
+                                }
+                            ]} />
+                    </section>
 
-                           }}
-                           markers={[
-                               {
-                                   lat: ride.latitude,
-                                   lng: ride.longitude
-                               }
-                           ]} />
-                   </RenderAfterNavermapsLoaded>
-                </section>
-
-                <section className="main-container">
-                    <div className="ride-header">
-                        <div className="ride-difficulty">{ ride.difficulty }</div>
-                    </div>
-
-                    <div className="ride-content">
-                        <div className="content-group ride-title">
-                            <h2>{ ride.name }</h2>
+                    <section className="main-container">
+                        <div className="ride-header">
+                            <div className="ride-difficulty">{ ride.difficulty }</div>
                         </div>
 
-                        <ul className="content-group user-info">
-                            <li>
-                                <span>개설자</span>
-                                <p>{ ride.user.name }</p>
-                            </li>
-                            <li>
-                                <span>이메일</span>
-                                <p>{ ride.user.email }</p>
-                            </li>
-                            <li>
-                                <span>전화번호</span>
-                                <p>{ ride.user.phone }</p>
-                            </li>
-                        </ul>
+                        <div className="ride-content">
+                            <div className="content-group ride-title">
+                                <h2>{ ride.name }</h2>
+                            </div>
 
-                        <ul className="content-group ride-detail">
-                            <li>
-                                <span>거리</span>
-                                <p>{ ride.distance }km</p>
-                            </li>
-                            <li>
-                                <span>고도</span>
-                                <p>
-                                    { ride.altitude }
-                                    { ride.altitude_detail != '' &&
-                                        <span> {ride.altitude_detail}m</span>
-                                    }
-                                </p>
-                            </li>
-                            <li>
-                                <span>시작시간</span>
-                                <p>{ ride.started_at }</p>
-                            </li>
-                            <li>
-                                <span>종료시간</span>
-                                <p>{ ride.ended_at }</p>
-                            </li>
-                            <li>
-                                <span>장소</span>
-                                <p>{ ride.address }</p>
-                            </li>
-
-                            { ride.address_detail != '' &&
+                            <ul className="content-group user-info">
                                 <li>
-                                    <span>장소상세</span>
-                                    <p>{ ride.address_detail }</p>
+                                    <span>개설자</span>
+                                    <p>{ ride.user.name }</p>
                                 </li>
+                                <li>
+                                    <span>이메일</span>
+                                    <p>{ ride.user.email }</p>
+                                </li>
+                                <li>
+                                    <span>전화번호</span>
+                                    <p>{ ride.user.phone }</p>
+                                </li>
+                            </ul>
+
+                            <ul className="content-group ride-detail">
+                                <li>
+                                    <span>거리</span>
+                                    <p>{ ride.distance }km</p>
+                                </li>
+                                <li>
+                                    <span>고도</span>
+                                    <p>
+                                        { ride.altitude }
+                                        { ride.altitude_detail != '' &&
+                                            <span> {ride.altitude_detail}m</span>
+                                        }
+                                    </p>
+                                </li>
+                                <li>
+                                    <span>시작시간</span>
+                                    <p>{ ride.started_at }</p>
+                                </li>
+                                <li>
+                                    <span>종료시간</span>
+                                    <p>{ ride.ended_at }</p>
+                                </li>
+                                <li>
+                                    <span>장소</span>
+                                    <p>{ ride.address }</p>
+                                </li>
+
+                                { ride.address_detail &&
+                                    <li>
+                                        <span>장소상세</span>
+                                        <p>{ ride.address_detail }</p>
+                                    </li>
+                                }
+                            </ul>
+
+                            { ride.file &&
+                                <div className='content-group'>
+                                    <Map id={'gpx-map'}
+                                        width={'100%'}
+                                        height={'360px'}
+                                        disabled={true}
+                                        zoom={14}
+                                        gpx={ ride.file } />
+                                </div>
                             }
-                        </ul>
 
-                        <div className="content-group ride-description">
-                            <p>
-                                { ride.description }
-                            </p>
+                            <div className="content-group ride-description">
+                                <p>
+                                    { ride.description }
+                                </p>
+                            </div>
+
+                            <div className="content-group ride-capacity">
+                                <div>정원 { ride.capacity }명</div>
+                                <div>현재 { this.state.participant_count }명 참석</div>
+                            </div>
                         </div>
 
-                        <div className="content-group ride-capacity">
-                            <div>정원 { ride.capacity }명</div>
-                            <div>현재 { this.state.participant_count }명 참석</div>
+                        <div className="btn-area">
+                            <button type="button"
+                                className="btn-attend"
+                                onClick={ this.handleSubmit }>참가 하기</button>
                         </div>
-                    </div>
-
-                    <div className="btn-area">
-                        <button type="button"
-                            className="btn-attend"
-                            onClick={ this.handleSubmit }>참가 하기</button>
-                    </div>
-                </section>
+                    </section>
+                </RenderAfterNavermapsLoaded>
             </main>
         );
     }
