e13c99677994c61cec6be37383cff0b7c907679c
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-09-05 17:35:02 +0900

    사이드 바 작업
    - 디자인 작업
    - 데이터 출력 작업
    - 이벤트 작업

--- resources/js/components/Aside.js
+++ resources/js/components/Aside.js
@@ -16,29 +16,56 @@ class Aside extends Component {
         };
     }
 
+    handleLogout(e) {
+        e.preventDefault();
+
+        storage.set('loggedToken', '');
+        alert('로그아웃 되었습니다.');
+        this.props.history.push('/');
+    }
+
     render() {
+        let user = this.props.state.user.user;
+        let isLoggedIn = this.props.state.user.isLoggedIn;
+
         return (
-            <aside className="aside">
+            <aside className={`aside ${this.props.showAside ? 'show' : ''}`}>
                 <div className="top-area">
-                    <Link to="/" className="btn-home">홈</Link>
-                    <button type="button" className="btn-aside-close">닫기</button>
+                    <button type="button" className="btn-aside-close"
+                        onClick={this.props.handleAside}>닫기</button>
                 </div>
 
                 <div className="user-info-area">
-                    <img src="/images/default_profile.png" className="user-profile" alt="프로필 이미지" />
+                    <img src="/images/global/default_profile.png" className="user-profile" alt="프로필 이미지" />
                     <p className="user-name">
-                        <span>ㅁㄴㅇ</span> 님
+                        { isLoggedIn ? `${user.name} 님` : '비로그인' }
                     </p>
                 </div>
 
                 <nav className="nav-area">
-                    <ul className="nav-list">
-                        <li><Link to="/">123123</Link></li>
-                    </ul>
+                    { isLoggedIn ?
+                        <ul className="nav-list">
+                            <li><Link to="/">메인</Link></li>
+                            <li><Link to="/ride/create">라이드 개설</Link></li>
+                            <li><Link to="/account/attend">신청내역</Link></li>
+                            <li><Link to="/account/create">개설내역</Link></li>
+                        </ul>
+                        :
+                        <ul className="nav-list">
+                            <li><Link to="/">메인</Link></li>
+                            <li><Link to="/login">로그인</Link></li>
+                            <li><Link to="/register">회원가입</Link></li>
+                        </ul>
+                    }
                 </nav>
 
                 <div className="btn-area">
-                    <button type="button" className="btn-logout">로그아웃</button>
+                    { isLoggedIn ?
+                        <button type="button" className="btn-logout"
+                            onClick={this.handleLogout}>로그아웃</button>
+                        :
+                        <Link to="/login" className="btn-login">로그인</Link>
+                    }
                 </div>
             </aside>
         );
