7fe8564dd13c682533e7ee5f3327985ae4445edd
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-09-06 17:59:10 +0900

    개설 내역 작업
    - 디자인 작업
    - 리스트 출력 작업

--- resources/js/pages/account/Attend.js
+++ resources/js/pages/account/Attend.js
@@ -1,5 +1,6 @@
 import React, {Component, useState} from 'react';
 import { connect } from 'react-redux';
+import {Link} from 'react-router-dom';
 
 import '@sass/pages/account/account.scoped.scss';
 
@@ -12,14 +13,98 @@ class Attend extends Component {
         super(props);
 
         this.state = {
+            rides: [],
+            page: 0,
+            isEnd: false
+        }
+
+        this.handleScroll = this.handleScroll.bind(this);
+        this.getData = this.getData.bind(this);
+    }
+
+    handleScroll(e) {
+        if (this.state.isEnd) {
+            let scrollPosition = e.srcElement.scrollingElement.scrollTop + window.innerHeight;
+
+            if (scrollPosition >= document.body.offsetHeight) {
+                this.getData();
+            }
         }
     }
 
+    getData() {
+        this.setState({
+            page: ++this.state.page,
+            isEnd: false
+        }, () => {
+            axios.get(`/api/account/attend?page=${this.state.page}`).then(res => {
+                let resData = res.data.rides.data;
+                let data = this.state.rides.concat(resData);
+
+                if (resData.length < 10) {
+                    window.removeEventListener('scroll', this.handleScroll);
+                }
+
+                this.setState({
+                    rides: data,
+                    isEnd: true
+                });
+            }).catch(err => {
+                console.log(err);
+            });
+        });
+    }
+
+    componentDidMount() {
+        this.getData();
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
     render() {
+        let rides = Array.from(this.state.rides);
+
         return (
             <main className="main">
                 <section className="attend-container">
-                    신청내역
+                    <h2 className="title">신청내역</h2>
+
+                    <ul className="ride-list">
+                        { rides.map((ride) => {
+                            return (
+                                <li key={ride.id}>
+                                    <Link to={`/ride/${ride.id}`}>
+                                        <div className="ride-header">
+                                            <h2 className="ride-title">{ ride.name }</h2>
+                                            <span className="ride-difficulty">{ ride.difficulty }</span>
+                                        </div>
+
+                                        <ul className="ride-detail">
+                                            <li>
+                                                <span>출발시간</span>
+                                                <p>
+                                                    { ride.started_at }
+                                                </p>
+                                            </li>
+                                            <li>
+                                                <span>종료시간</span>
+                                                <p>
+                                                    { ride.ended_at || '미정' }
+                                                </p>
+                                            </li>
+                                        </ul>
+
+                                        <div className="btn-area">
+                                            <button type="button">취소하기</button>
+                                        </div>
+                                    </Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
                 </section>
             </main>
         );
