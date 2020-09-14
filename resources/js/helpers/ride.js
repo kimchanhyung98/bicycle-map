7a344ac574745fa36f11b364b77bc8dd3766e58a
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-09-14 22:21:00 +0900

    Create ride.js

---
+++ resources/js/helpers/ride.js
@@ -0,0 +1,12 @@
+export function formatDifficulty(difficulty) {
+    switch (difficulty) {
+        case 'beginner':
+            return '초보자'
+        case 'intermediate':
+            return '중급자'
+        case 'advanced':
+            return '숙련자'
+        default:
+            return '미정'
+    }
+}
