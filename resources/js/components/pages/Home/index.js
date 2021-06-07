44092be3e1c2f6a8572dd867b7cfe89bed3e6761
LeeByeongMuk <mansogod@gmail.com>
2021-06-08 01:07:03 +0900

    Modify Home Page Component
    #51

---
+++ resources/js/components/pages/Home/index.js
@@ -0,0 +1,60 @@
+import React, {memo, useCallback, useEffect, useState} from "react";
+import PageTemplate from "@components/templates/PageTemplate";
+import Header from "@components/UI/organisms/Header";
+import Aside from "@components/UI/organisms/Aside";
+import RideLinkedList from "@components/UI/organisms/RideLinkedList";
+
+const Home = memo(() => {
+    const [rides, setRides] = useState([]);
+    const [page, setPage] = useState(0);
+    const [isEnd, setIsEnd] = useState(false);
+
+    const handleScroll = useCallback((event) => {
+        if (isEnd) {
+            let scrollPosition = event.srcElement.scrollingElement.scrollTop + window.innerHeight;
+
+            if (scrollPosition >= document.body.offsetHeight) {
+                this.getData();
+            }
+        }
+    }, []);
+
+    const getData = useCallback(() => {
+        setIsEnd(false, () => {
+            axios.get(`/api/ride?page=${page + 1}`).then(res => {
+                const resData = res.data.rides.data;
+                const data = rides.concat(resData);
+
+                if (resData.length < 10) {
+                    window.removeEventListener('scroll', this.handleScroll);
+                }
+
+                setRides(data);
+                setPage((prevPage) => prevPage + 1);
+                setIsEnd(true);
+            }).catch(err => {
+                console.log(err);
+            });
+        });
+    }, []);
+
+    useEffect(() => {
+        getData();
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
+
+    return (
+        <PageTemplate Header={Header}
+                      Aside={Aside}>
+            <section>
+                <RideLinkedList rides={rides} />
+            </section>
+        </PageTemplate>
+    )
+});
+
+export default Home;
