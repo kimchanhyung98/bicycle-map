3e7c97dc2d3a9ae139ca323180f0f2bd88d22b7f
LeeByeongMuk <mansogod@gmail.com>
2021-06-11 02:33:22 +0900

    Modify Map Component
    #51

---
+++ resources/js/components/UI/atoms/Map/index.js
@@ -0,0 +1,75 @@
+import React, {memo, useCallback, useEffect} from "react";
+import {RenderAfterNavermapsLoaded, NaverMap, Marker} from "react-naver-maps";
+
+const NAVER_API_KEY = env.NCLOUD_CLIENT_ID;
+const location = {
+    latitude: '37.554722',
+    longitude: '126.970833'
+}
+
+const Map = memo(({
+    mapOptions,
+    markers,
+    ...props
+}) => {
+    const navermaps = window.naver.maps;
+    const {id, style, center, zoom} = mapOptions;
+    const {disabled, setLocation, gpx} = props;
+    let mapRef;
+
+    const handleClick = useCallback((event) => {
+        if (!disabled) {
+            const {x, y} = event.coord;
+            setLocation(y, x);
+        }
+    }, [disabled, setLocation]);
+
+    const startDataLayer = useCallback((xmlDoc) => {
+        mapRef.instance.data.addGpx(xmlDoc);
+    }, []);
+
+    useEffect(() => {
+        if (gpx) {
+            window.naver.maps.Event.once(mapRef.instance, 'init_stylemap', () => {
+                $.ajax({
+                    url: gpx.path,
+                    dataType: 'xml',
+                    success: (res) => {
+                        startDataLayer(res);
+                    }
+                });
+            })
+        }
+    }, []);
+
+    return (
+        <RenderAfterNavermapsLoaded ncpClientId={NAVER_API_KEY}
+                                    error={<p>오류</p>}
+                                    loading={<p>Loading</p>}>
+            <NaverMap {...mapOptions}
+                      id={id || 'map'}
+                      style={{
+                          width: style.width || '100%',
+                          height: style.height || '300px'
+                      }}
+                      defaultCenter={center || location}
+                      center={center || location}
+                      zoom={zoom || 12}
+                      naverRef={ref => {
+                          mapRef = ref;
+                      }}>
+
+                {markers &&
+                    markers.map((marker) => {
+                        return (
+                            <Marker key={marker}
+                                    position={marker}/>
+                        )
+                    })
+                }
+            </NaverMap>
+        </RenderAfterNavermapsLoaded>
+    )
+});
+
+export default Map;
