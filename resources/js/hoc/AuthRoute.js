067e0099b1f32b82641bbee4a7ee18992b21ff0c
LeeByeongMuk <mansogod@gmail.com>
2021-06-24 00:55:40 +0900

    Update AuthRoute.js

--- resources/js/hoc/AuthRoute.js
+++ resources/js/hoc/AuthRoute.js
@@ -2,10 +2,15 @@ import React, {useEffect} from "react";
 import {Route, useHistory} from 'react-router-dom';
 import storage from "@/utils/storage";
 
+import {getUserStatus} from "@/api/userApi";
+
 const AuthRoute = ({check = null, ...props}) => {
     const history = useHistory();
 
     const authCheck = async () => {
+        const response = await getUserStatus();
+        console.log(response);
+
         if (check === null) return;
 
         try {
