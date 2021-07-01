7374e48a2a9b52ae242acabeb9c9ca4bb029ec3d
LeeByeongMuk <mansogod@gmail.com>
2021-07-01 21:30:47 +0900

    Update Comment Reply Form Event

--- resources/js/components/UI/molecules/CommentContent/index.js
+++ resources/js/components/UI/molecules/CommentContent/index.js
@@ -57,7 +57,7 @@ const CommentButtonWrapper = styled.div`
 
     button {
         background: none;
-        font-size: ${font.sizeBase};
+        font-size: ${font.sizeSmall};
         text-decoration: underline;
     }
 `;
@@ -127,7 +127,7 @@ const CommentContent = memo(({commentData, setIsShow, setComments, ...props}) =>
 
                     <CommentButtonWrapper>
                         {!parent_id ?
-                            <Button onClick={handleSetIsShow}>대댓글</Button> : <div></div>
+                            <Button onClick={handleSetIsShow}>답글쓰기</Button> : <div></div>
                         }
                         { user.id === userId ?
                             <Button onClick={handleCommentDelete}>삭제</Button> : ''
